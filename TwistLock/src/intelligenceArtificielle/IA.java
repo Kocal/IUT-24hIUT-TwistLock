604ddd4165466c0576df27d38e353de19d7591e9
Emilien <Emilien@172.30.8.5>
2015-05-29 15:13:38 +0200

    ajout IA

---
+++ TwistLock/src/intelligenceArtificielle/IA.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intelligenceArtificielle;
+
+/**
+ *
+ * @author Emilien
+ */
+public class IA {
+    
+    
+    /*
+    10- ah vous de jouer (Vert)
+    20: coup adversaire: 9B1
+    
+    */
+    
+    // index correspond au type de message (20, 21, 22, 50, 88)
+    public void posAdversaire(String index){
+        
+        if(true){
+            
+        }
+    }
+    public void setCoin(String c){
+        
+        
+    }
+}

7131b80fd0e0ecdbb45273d0921a1fc343ee2b1c
ADussauge <aurelien.dussauge@univ-lyon1.fr>
2015-05-29 19:28:19 +0200

    Ajout d'une fonction retournant une chaine de caractère à partir du point

--- TwistLock/src/intelligenceArtificielle/Point.java
+++ TwistLock/src/intelligenceArtificielle/Point.java
@@ -5,6 +5,10 @@
  */
 package intelligenceArtificielle;
 
+import tablier.Coin;
+import tablier.Conteneur;
+import tablier.Tablier;
+
 /**
  *
  * @author Aurélien
@@ -21,5 +25,29 @@ public class Point {
         this.colonne = colonne;
     }
     
-    
+    public String getChaine(Tablier t)
+    {
+        Coin[][] coins = t.getCoins();
+        Coin c = coins[ligne][colonne];
+        for(int i=0;i<t.getHauteur();i++)
+        {
+            for(int j=0;j<t.getLargeur();j++)
+            {
+                Conteneur cont = t.getConteneur(i, j);
+                for(int k=0;k<cont.getCoins().size();k++)
+                {
+                    if(cont.getCoins().get(k) == c)
+                    {
+                        try {
+                            return cont.getPositionAfficher(k+1);
+                        }
+                        catch (Exception ex) {
+                            System.err.println(ex);
+                        }
+                    }
+                }
+            }
+        }
+        return "0A1";
+    }
 }

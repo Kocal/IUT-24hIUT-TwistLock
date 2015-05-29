1d67f1e8c6de939976620d3eaf6752fa13b98e3a
ADussauge <aurelien.dussauge@gmail.com>
2015-05-29 15:06:05 +0200

    Ajout du contenu de la classe Conteneur

--- TwistLock/src/tablier/Conteneur.java
+++ TwistLock/src/tablier/Conteneur.java
@@ -5,5 +5,24 @@ package tablier;
  * @author Kocal
  */
 public class Conteneur {
+    public int ligne;
+    public int colonne;
+    public int valeur;
     
+    public String getColonneAfficher(){
+        return Character.toString((char)('A'+colonne));
+    }
+    
+    public String getLigneAfficher(){
+        return Integer.toString(ligne+1);
+    }
+    
+    public String getPositionAfficher(int coin) throws Exception
+    {
+        if(coin<1 || coin>4)
+        {
+            throw new Exception("Erreur : Le numéro de coin doit être dans l'intervalle 1-4");
+        }
+        return this.getLigneAfficher()+this.getColonneAfficher()+Integer.toString(coin);
+    }
 }

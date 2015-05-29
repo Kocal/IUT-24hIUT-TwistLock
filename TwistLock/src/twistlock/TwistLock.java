c781c687521469815f51bc12d7f794c27aa73cc7
ALLIAUME Hugo <kocal@live.fr>
2015-05-29 14:21:27 +0200

    Premier commit.

---
+++ TwistLock/src/twistlock/TwistLock.java
@@ -0,0 +1,16 @@
+package twistlock;
+
+/**
+ *
+ * @author Kocal
+ */
+public class TwistLock {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

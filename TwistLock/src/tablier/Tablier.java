2a779fb38353454caac780e9be225264a3794321
Benjamin <Benjamin@172.30.7.66>
2015-05-29 15:33:28 +0200

    initGrille(String rawMap) initialise la grille via la chaine de description envoyé par le serveur  (MAP=...|...| )

--- TwistLock/src/tablier/Tablier.java
+++ TwistLock/src/tablier/Tablier.java
@@ -58,8 +58,25 @@ public class Tablier {
      * Initialise une grille de dimension "hauteur" * "largeur"
      * @param grilleChaine 
      */
-    private void initGrille(String grilleChaine) {
+    private void initGrille(String rawMap) {
         
+        this.grille = new Conteneur[this.hauteur][this.largeur];
+
+        //rawMap = "MAP=1:2:3:4:5:6:7:8:9:10|11:12:13:14:15:16:17:18:19:20|21:22:23:24:25:26:27:28:29:30|";
+
+        String[] grid_lines = rawMap.substring(4).split("\\|");
+
+        for (int j = 0; j < grid_lines.length; j++) {
+
+            String[] grid_column = grid_lines[j].split(":");
+
+            for (int i = 0; i < grid_column.length; i++) {
+
+                this.grille[i][j] = new Conteneur(i, j, Integer.valueOf(grid_column[i]));
+
+            }
+        }
+
     }
     
     /**

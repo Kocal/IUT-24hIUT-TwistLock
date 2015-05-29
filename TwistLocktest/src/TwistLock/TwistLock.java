ae78e4c79b166e4fd7059ee5c5d7da999c51dfa8
Embraser01 <embraser01@gmail.com>
2015-05-29 14:45:23 +0200

    Test 2.0

--- TwistLocktest/src/TwistLock/TwistLock.java
+++ TwistLocktest/src/TwistLock/TwistLock.java
@@ -10,6 +10,6 @@ public class TwistLock {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here dsds
+        // TODO code application logic here
     }
 }
